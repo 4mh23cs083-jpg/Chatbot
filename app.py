7e3edc44bd54c83df951ea49f99deef91213a5f1
Manvanth M <4mh23cs083@gmail.com>
2026-02-05 18:59:29 +0530

    Chatbot

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from ai import get_ai_response
-print("Welcome to the chatbot!")
-print("Type 'exit' to quit.")
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
-    ai_response = get_ai_response(user_input)
-    print(f"AI: {ai_response}")
+#from ai import get_ai_response
+#print("Welcome to the chatbot!")
+#print("Type 'exit' to quit.")
+#while True:
+#    user_input = input("You: ")
+#    if user_input.lower() == "exit":
+#        break
+#    ai_response = get_ai_response(user_input)
+#    print(f"AI: {ai_response}")
 
 
 
-#from flask import  Flask
-#app = Flask(__name__)
-#@app.route('/')
-#def index():
-#    return 'Hello World!'
-#if __name__ == '__main__':
-#    app.run(debug=True)
+from flask import  Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello World!'
+if __name__ == '__main__':
+    app.run(debug=True)
  
